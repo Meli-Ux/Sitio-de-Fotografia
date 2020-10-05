0d272644ed67d104065da108ecd62ea773e245bb
meli2528 <72412321+meli2528@users.noreply.github.com>
2020-10-05 17:31:08 -0300

    Add files via upload

---
+++ js/effects.js
@@ -0,0 +1,8 @@
+const txtSearch = document.querySelector(".txt-search");
+const btnSearch = document.querySelector(".fa-search");
+
+
+btnSearch.onclick = function(){
+    txtSearch.classList.toggle("txt-search-w");
+}
+
